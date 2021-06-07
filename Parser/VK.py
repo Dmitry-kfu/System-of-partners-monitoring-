54e637aa51125dba129a981699320e05e3d81ad1
Дмитрий Кузьмин <64068399+Dmitry-kfu@users.noreply.github.com>
2021-06-07 19:30:23 +0300

    Update VK.py

--- Parser/VK.py
+++ Parser/VK.py
@@ -8,4 +8,3 @@ session = vk.Session(access_token=token)
 api = vk.API(session, v='11.9.1')
 person = api.users.get(user_ids=ids, fields='about, education, home_town, exports, interests, bdate')
 print(person)
-#'pavlovsemen'
